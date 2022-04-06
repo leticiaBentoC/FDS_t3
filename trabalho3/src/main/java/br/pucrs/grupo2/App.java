0d965c97fe48427e16c530ac23f01d4976bf0428
Letícia Bento do Carmo <leticiac@dbserver.com.br>
2022-04-06 17:28:53 -0300

    Commmit da Leticia, add println

--- trabalho3/src/main/java/br/pucrs/grupo2/App.java
+++ trabalho3/src/main/java/br/pucrs/grupo2/App.java
@@ -38,6 +38,8 @@ public class App {
                 System.out.println("Cor : " + cor);
                 System.out.println("Categoria : " + categoria);
                 System.out.println("---------------\n\n");
+
+                System.out.println("Leticia Bento do Carmo\n\n");
             }
         }
     }
